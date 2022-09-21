0ec2bc91dd15f074b022f28dc5910da37dd5d5c9
sandro <sandrosilva9029@gmail.com>
2022-09-21 17:20:23 -0300

    adicionado getit, troca de pagina e highlight dos titulos com store

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/screens/base/base_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 import '/keys/constaints.dart'; //keys
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeParse();
+  setupLocators();
   runApp(const MyApp());
+}
 
+void setupLocators(){
+  GetIt.I.registerSingleton(PageStore());
 }
 
 Future<void> initializeParse() async {
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
 
+import '../../stores/page_store.dart';
 import '../home/home_screen.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   BaseScreen({Key? key}) : super(key: key);
 
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
+
+  final PageStore pageStore = GetIt.I<PageStore>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    reaction(
+      (_) => pageStore.page,
+      (page) => pageController.jumpToPage(page),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
