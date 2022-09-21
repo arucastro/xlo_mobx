1e5c7b62bc1f28816f0e6e3a17fea976768392c1
sandro <sandrosilva9029@gmail.com>
2022-09-21 16:19:09 -0300

    visual do drawer customizado

--- lib/components/custom_drawer/custom_drawer.dart
+++ lib/components/custom_drawer/custom_drawer.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:xlo_mobx/components/custom_drawer/page_section.dart';
+
+import 'custom_drawer_header.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: MediaQuery.of(context).size.width * 0.7,
-        child: Drawer(),
+    return ClipRRect(
+      borderRadius: const BorderRadius.horizontal(right: Radius.circular(40)),
+      child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: Drawer(
+            child: ListView(
+              children: [
+                CustomDrawerHeader(),
+                PageSection(),
+              ],
+            ),
+          ),
+      ),
     );
   }
 }
