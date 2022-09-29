7611a38f83eafbc76117217f9881631ceb8678e6
sandro <sandrosilva9029@gmail.com>
2022-09-29 14:12:40 -0300

    campo de pesquisa na tela inicial finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:xlo_mobx/repositories/cep_repository.dart';
 import 'package:xlo_mobx/repositories/ibge_repository.dart';
 import 'package:xlo_mobx/screens/base/base_screen.dart';
 import 'package:xlo_mobx/stores/category_store.dart';
+import 'package:xlo_mobx/stores/home_store.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
 import 'package:xlo_mobx/stores/user_manager_store.dart';
 import '/keys/constaints.dart'; //keys
@@ -19,6 +20,7 @@ void main() async {
 
 void setupLocators(){
   GetIt.I.registerSingleton(PageStore());
+  GetIt.I.registerSingleton(HomeStore());
   GetIt.I.registerSingleton(UserManagerStore());
   GetIt.I.registerSingleton(CategoryStore());
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+import 'package:xlo_mobx/stores/home_store.dart';
 
 import '../../components/custom_drawer/custom_drawer.dart';
 import 'components/search_dialog.dart';
@@ -8,26 +11,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final homeStore = GetIt.I<HomeStore>();
 
-    openSearch(BuildContext context) async{
-     final search = await showDialog(context: context, builder: (_) => SearchDialog());
-     print(search);
+    openSearch(BuildContext context) async {
+      final search = await showDialog(
+        context: context,
+        builder: (_) => SearchDialog(
+          currentSearch: homeStore.search,
+        ),
+      );
+      if (search != null) homeStore.setSearch(search);
+      print(homeStore.search);
     }
 
     return SafeArea(
       child: Scaffold(
         drawer: CustomDrawer(),
         appBar: AppBar(
+          centerTitle: true,
+          title: Observer(builder: (_){
+            if (homeStore.search!.isEmpty){
+              return Container();
+            }else {
+              return GestureDetector(
+                onTap: () => openSearch(context),
+                child: LayoutBuilder(
+                  builder: (_, constraints){
+                    return Container(
+                      width: constraints.biggest.width,
+                      child: Text(homeStore.search!),
+                    );
+                  },
+                ),
+              );
+            }
+          }),
           actions: [
-            IconButton(
-              onPressed: () {
-                openSearch(context);
-              },
-              icon: const Icon(
-                Icons.search,
-                size: 30,
-              ),
-            ),
+            Observer(builder: (_){
+              if(homeStore.search!.isEmpty){
+                return IconButton(
+                  onPressed: () {
+                    openSearch(context);
+                  },
+                  icon: const Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                );
+              }
+              return IconButton(
+                onPressed: () {
+                  homeStore.setSearch('');
+                },
+                icon: const Icon(
+                  Icons.close,
+                  size: 30,
+                ),
+              );
+            })
           ],
         ),
       ),
