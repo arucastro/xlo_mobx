fef6a9ff6041bd1e3868378c3928100130727dba
sandro <sandrosilva9029@gmail.com>
2022-09-29 13:42:30 -0300

    adicionado campo de pesquisa na tela inicial

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 import '../../components/custom_drawer/custom_drawer.dart';
+import 'components/search_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    openSearch(BuildContext context) async{
+     final search = await showDialog(context: context, builder: (_) => SearchDialog());
+     print(search);
+    }
+
     return SafeArea(
       child: Scaffold(
         drawer: CustomDrawer(),
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                openSearch(context);
+              },
+              icon: const Icon(
+                Icons.search,
+                size: 30,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
